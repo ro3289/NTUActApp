26871cc5f27a53eb9fb8e284153719ae2573e3d8
janelai22 <janelai22@gmail.com>
2014-01-15 09:52:54 +0800

    nothing

--- src/com/example/eventmap/GoogleFragment.java
+++ src/com/example/eventmap/GoogleFragment.java
@@ -2,8 +2,8 @@ package com.example.eventmap;
 
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
